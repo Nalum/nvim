e9273b39c9629d8a0349850eaa95b8a36365be21
Luke Mallon (Nalum) <luke@mallon.ie>
2022-12-21 13:21:51 +0000

    Change signature hint key to remove conflict
    Signed-off-by: Luke Mallon (Nalum) <luke@mallon.ie>

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -39,7 +39,7 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
