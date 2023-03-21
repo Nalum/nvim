ea916193cbb849a8d2b35fca203efd9ce3d963ad
Luke Mallon (Nalum) <luke@mallon.ie>
2023-03-21 21:13:28 +0000

    Update config to stop using depricated config
    Signed-off-by: Luke Mallon (Nalum) <luke@mallon.ie>

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,8 +1,6 @@
 require'nvim-tree'.setup({
   sort_by = "case_sensitive",
   disable_netrw = true,
-  open_on_setup = true,
-  ignore_buffer_on_setup = true,
   view = {
     adaptive_size = true,
     mappings = {
@@ -23,3 +21,19 @@ require'nvim-web-devicons'.setup({
   color_icons = true,
   default = true,
 })
+
+local function open_nvim_tree(data)
+  -- buffer is a real file on the disk
+  local real_file = vim.fn.filereadable(data.file) == 1
+  -- buffer is a [No Name]
+  local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+  if not real_file and not no_name then
+    return
+  end
+
+  -- open the tree, find the file but don't focus it
+  require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
+end
+
+vim.api.nvim_create_autocmd({"VimEnter"}, {callback = open_nvim_tree })
